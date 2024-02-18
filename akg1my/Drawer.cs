45dc194ed5e07d6921209fc5ae7176dc67773673
Denis Shafarenko <phisika001@mail.ru>
2024-02-18 14:49:16 +0300

    drawer

---
+++ akg1my/Drawer.cs
@@ -0,0 +1,139 @@
+﻿using System.Drawing;
+using System.Numerics;
+using System.Security.Cryptography.Pkcs;
+
+namespace akg1my
+{
+    internal class Drawer
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Stride { get; set; }
+
+        public unsafe byte* Data {  get; set; }
+
+        public unsafe Drawer(int width, int height)
+        {
+            Width = width; 
+            Height = height;
+        }
+
+        public bool PointInWindow(int x, int y)
+        {
+            /*if (x > 0 && y > 0 && x < width && y < height)
+                Console.WriteLine($"{x}, {y}, {width}, {height}, {x > 0 && y > 0 && x < width && y < height}");*/
+            return x > 0 && y > 0 && x < Width && y < Height;
+        }
+
+        public unsafe void DrawLine(int x0, int y0, int x1, int y1, Color color)
+        {
+            /*Console.WriteLine($"draw line from ({x0}, {y0}) to ({x1}, {y1})");*/
+
+            bool step = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+
+            if (step)
+            {
+                (x0, y0) = (y0, x0);
+                (x1, y1) = (y1, x1);
+            }
+
+            if (x0 > x1)
+            {
+                (x0, x1) = (x1, x0);
+                (y0, y1) = (y1, y0);
+            }
+
+            int dx = x1 - x0;
+            int dy = Math.Abs(y1 - y0);
+            int error = dx / 2;
+            int ystep = (y0 < y1) ? 1 : -1;
+            int y = y0;
+            int row, col;
+
+            for (int x = x0; x <= x1; x++)
+            {
+                if (step)
+                {
+                    row = x;
+                    col = y;
+                }
+                else
+                {
+                    row = y;
+                    col = x;
+                }
+
+                byte* pixelPtr = Data + row * Stride + col * 3;
+                *(pixelPtr++) = color.B;
+                *(pixelPtr++) = color.G;
+                *(pixelPtr) = color.R;
+
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+        }
+
+        // i - independent value, d - dependent value
+        protected List<int> Interpolate(int i0, int d0, int i1, int d1)
+        {
+            var values = new List<int>();
+            float slope = (d1 - d0) / (i1 - i0);
+            float d = d0;
+
+            for (float i = i0; i <= i1; i += slope)
+            {
+                values.Add((int)d);
+                d += slope;
+            }
+            return values;
+        }
+
+        public unsafe void RasterizeTriangle(Point v0, Point v1, Point v2, Color color)
+        {
+            if (v1.Y < v0.Y) (v1, v0) = (v0, v1);
+            if (v2.Y < v0.Y) (v2, v0) = (v0, v2);
+            if (v2.Y < v1.Y) (v2, v1) = (v1, v2);
+
+            int x0 = v0.X, x1 = v1.X, x2 = v2.X;
+            int y0 = v0.Y, y1 = v1.Y, y2 = v2.Y;
+
+            var x01 = Interpolate(y0, x0, y1, x1);
+            var x12 = Interpolate(y1, x1, y2, x2);
+            var x02 = Interpolate(y0, x0, y2, x2);
+
+            x01.RemoveAt(x01.Count - 1);
+            var x012 = new List<int>(x01.Count + x12.Count);
+            x012.AddRange(x01);
+            x012.AddRange(x12);
+
+            int middle = x012.Count / 2;
+            List<int> x_left, x_right;
+            if (x02[middle] < x012[middle])
+            {
+                x_left = x02;
+                x_right = x012;
+            }
+            else
+            {
+                x_left = x012;
+                x_right = x02;
+            }
+
+            for (int y = y0; y <= y2; y++)
+            {
+                for (int x = x_left[y - y0]; x <= x_right[y - y0]; x++)
+                {
+                    byte* pixelPtr = Data + y * Stride + x * 3;
+
+                    *(pixelPtr++) = color.B;
+                    *(pixelPtr++) = color.G;
+                    *(pixelPtr) = color.R;
+                }
+            }
+        }
+    }
+}
